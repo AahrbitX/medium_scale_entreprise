c72176c761223bb59165eded343c7d38eba59c4b
karthikeyan <karthikeyanj1915@gmail.com>
2025-01-29 18:33:23 +0530

    build error free

--- app/store/[id]/page.tsx
+++ app/store/[id]/page.tsx
@@ -1,9 +1,34 @@
 import { products } from "@/static/product";
 import React from "react";
 
-function ProductIdPage({ params }: { params: { id: string } }) {
-  const product = products.find((product) => product.id === params.id);
-  return <div className="mt-24 test-section">{product?.name}</div>;
+interface ProductIdPageProps {
+  params: Promise<{ id: string }>;
+}
+
+export async function generateStaticParams() {
+  return products.map((product) => ({
+    id: product.id,
+  }));
+}
+
+export async function generateMetadata({ params }: ProductIdPageProps) {
+  const { id } = await params;
+  const product = products.find((product) => product.id === id);
+  return {
+    title: product?.name + " | MSE" || "Product not found",
+  };
+}
+
+async function ProductIdPage({ params }: ProductIdPageProps) {
+  const { id } = await params;
+
+  const product = products.find((product) => product.id === id);
+
+  if (!product) {
+    return <div className="mt-24 test-section">Product not found</div>;
+  }
+
+  return <div className="mt-24 test-section">{product.name}</div>;
 }
 
 export default ProductIdPage;
